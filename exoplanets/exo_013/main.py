f9a0c69d2859e4f67650e2feb720b30996ee84e9
miraceti <elecam@gmail.com>
2024-08-18 14:23:22 +0200

    d

--- exoplanets/exo_013/main.py
+++ exoplanets/exo_013/main.py
@@ -13,6 +13,7 @@ import json
 import datetime
 
 
+
 #######################
 urlexo1 = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+count(pl_name)+as+nbe+from+ps+where+default_flag=1&format=json"
 print(urlexo1)
