46acebe8e218d3b011fa106006c110518aa1753c
miraceti56 <elecam@gmail.com>
2025-04-16 22:51:46 +0200

    25

--- fletpages/fletpages1/main.py
+++ fletpages/fletpages1/main.py
@@ -1,1 +1,23 @@
+import flet as ft
+from fletx import Xapp, route
+from views.home_view import HomeView
+from views.next_view import NextView
+from views.page_001 import page_001
+from views.page_002 import page_002
+from views.page_003 import page_003
 
+def main(page:ft.Page):
+    page.title=" FletX Routing Example"
+
+    Xapp(
+        page=page,
+        routes=[
+            route(route="/", view=HomeView),
+            route(route="/page_001", view=page_001),
+            route(route="/page_002", view=page_002),
+            route(route="/page_003", view=page_003),
+        ]
+    )
+    
+
+ft.app(target=main)
